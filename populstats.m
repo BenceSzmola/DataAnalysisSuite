e4e0791a7110e8011e91c7a11506641287851bd4
BenceSzmola <0szmola.bence0@gmail.com>
2019-09-26 16:06:22 +0200

    getting data out of csvs

--- populstats.m
+++ populstats.m
@@ -1,19 +1,35 @@
 function populstats
 
+% % % Reading CSVs into a 3D cell
 [fnames,path] = uigetfile('*.csv','Select the CSVs!','Multiselect','on');
 cd(path);
-temp={};
 for i = 1:length(fnames)
+    temp={};
     fileID = fopen(fnames{i});
-    line = [0];
-    display('before while')
-    while line(1) ~= -1
-        display('in while')
+    line = '';
+    while ischar(line)
         line = fgetl(fileID);
+        if ~ischar(line)
+            break
+        end
         line_c = {line};
-        display(line)
         temp = cat(1,temp,line_c);
     end
-    assignin('base','cell',temp);
+    assignin('base','temporo',temp);
+    alldata{:,:,i} = temp;
     fclose(fileID);
 end
+assignin('base','AD',alldata);
+
+poi = find(cellfun(@(x)ischar(x)&&contains(x,'Ca2+ detection'),alldata{:,:,1}));
+ROInums = textscan(alldata{:,:,1}{poi+1},'%d# ;');
+ROInums = ROInums{:};
+
+% % % Szinkron tüzelések mátrixa
+corrmat = zeros(length(ROInums));
+for i = 1:length(fnames)
+    poi = find(cellfun(@(x)ischar(x)&&contains(x,'Active'),alldata{:,:,i}))+2;
+    [~,pausespot] = textscan(alldata{:,:,i}{poi},'%*f ; ');
+    actives = cell2mat(textscan(alldata{:,:,i}{poi}(pausespot:end),'%d#'));
+end
+assignin('base','actives',actives);
