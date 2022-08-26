227b84b131c80cab3bb304420a9650aef197bc85
BenceSzmola <0szmola.bence0@gmail.com>
2022-08-26 18:09:30 +0200

    Update spectrogramMacher.m

--- spectrogramMacher.m
+++ spectrogramMacher.m
@@ -16,18 +16,19 @@ taxis = linspace(-round(length(data)*1000/(2*fs),4),round(length(data)*1000/(2*f
 for i = 1:size(data,1)
 %     normdog = dogged+abs(min(dogged(i,:)));
 %     normdog = normdog/max(normdog);
-    normData = data + abs(min(data(i,:)));
+    normData = data(i,:) + abs(min(data(i,:)));
     normData = normData / max(normData);
     [cfs,f] = cwt(data(i,:),'amor',fs,'FrequencyLimits',[1,1000]);
+    cfs = abs(cfs);
     
     figure('Name',['CWT Spectrogram'])
 %     surface('XData',taxis,'YData',f,'CData',abs(cfs),...
 %         'ZData',zeros(size(cfs)),'EdgeColor','none','CDataMapping','scaled')
-    surf(taxis,f,abs(cfs),'EdgeColor','interp')
+    surf(taxis,f,cfs,'EdgeColor','interp')
     view([0,90])
     hold on
 %     plot3(taxis,normdog*300+600,ones(size(taxis))*max(abs(cfs(:))),'-r')
-    plot3(taxis, normData*300 + 600, ones(size(taxis))*max(abs(cfs(:))), '-r')
+    plot3(taxis, normData*300 + 600, ones(size(taxis))*max(cfs(:)), '-r')
     hold off
     axis tight
     ylabel('Frequency [Hz]')
