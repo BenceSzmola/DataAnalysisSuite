8222ca03e083b5b71de1d44a5a1b6a550ed26bad
BenceSzmola <0szmola.bence0@gmail.com>
2022-09-05 14:52:02 +0200

    adjusted spectrogramMacher for dynamic freqLims

--- spectrogramMacher.m
+++ spectrogramMacher.m
@@ -1,13 +1,23 @@
-function spectrogramMacher(data,fs,w1,w2,detBorders)
+function spectrogramMacher(data,fs,freqLims,detBorders)
+
+if any(freqLims > (fs/2))
+    eD = errordlg('Frequency limits exceed Nyquist value!');
+    pause(1)
+    if ishandle(eD)
+        close(eD)
+    end
+    return
+end
 
 if size(data,1) > size(data,2)
     data = data';
 end
 
-if (w1 > 1000) | (w2 > 1000)
-    errordlg('Unexpected frequency cutoff values!')
-    return
-end
+freqRange = freqLims(2)-freqLims(1);
+% if (w1 > 1000) | (w2 > 1000)
+%     errordlg('Unexpected frequency cutoff values!')
+%     return
+% end
 
 % dogged = DoG(data,fs,w1,w2);
 taxis = linspace(-round(length(data)*1000/(2*fs),4),round(length(data)*1000/(2*fs),4),...
@@ -18,7 +28,7 @@ for i = 1:size(data,1)
 %     normdog = normdog/max(normdog);
     normData = data(i,:) + abs(min(data(i,:)));
     normData = normData / max(normData);
-    [cfs,f] = cwt(data(i,:),'amor',fs,'FrequencyLimits',[1,1000]);
+    [cfs,f] = cwt(data(i,:),'amor',fs,'FrequencyLimits',freqLims);
     cfs = abs(cfs);
     
     figure('Name', 'CWT Spectrogram', 'NumberTitle', 'off')
@@ -29,9 +39,9 @@ for i = 1:size(data,1)
     hold on
 
 %     plot3(taxis,normdog*300+600,ones(size(taxis))*max(abs(cfs(:))),'-r')
-    plot3(taxis, normData*300 + 600, ones(size(taxis))*max(cfs(:)), '-r')
+    plot3(taxis, normData*(0.3*freqRange) + 0.6*freqRange, ones(size(taxis))*max(cfs(:)), '-r')
     
-    if (nargin > 4) && ~isempty(detBorders)
+    if (nargin > 3) && ~isempty(detBorders)
         xline(taxis(detBorders(1)),'--w','LineWidth',1);
         xline(taxis(detBorders(2)),'--w','LineWidth',1);
     end
