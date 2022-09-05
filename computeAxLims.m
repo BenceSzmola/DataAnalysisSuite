0b7ebd8818f932eedf398e5958a5ce9e65eb2f2c
BenceSzmola <0szmola.bence0@gmail.com>
2022-09-05 12:00:23 +0200

    added custom y axis limits to DASeV

--- computeAxLims.m
+++ computeAxLims.m
@@ -1,19 +1,19 @@
 function axLims = computeAxLims(fullData, yLimMode, tAxis, winInds)
 
-    axLims = nan(size(fullData, 1), 2);
-    switch yLimMode
-        case 'global'
+    if isnumeric(yLimMode)
+        axLims = yLimMode;
+    else
+        axLims = nan(size(fullData, 1), 2);
+        
+        if strcmp('global', yLimMode)
             dataLen = length(tAxis);
             winInds = 1:dataLen;
-
-        case 'window'
-            dataLen = length(winInds);
-
-    end
+        end
         
-    axLims(:,1) = min(fullData(:,winInds), [], 2);
-    axLims(:,1) = axLims(:,1) - 0.1*abs(axLims(:,1));
-    axLims(:,2) = max(fullData(:,winInds), [], 2);
-    axLims(:,2) = axLims(:,2) + 0.1*abs(axLims(:,2));
-
+        axLims(:,1) = min(fullData(:,winInds), [], 2);
+        axLims(:,1) = axLims(:,1) - 0.1*abs(axLims(:,1));
+        axLims(:,2) = max(fullData(:,winInds), [], 2);
+        axLims(:,2) = axLims(:,2) + 0.1*abs(axLims(:,2));
+    end
+    
 end
