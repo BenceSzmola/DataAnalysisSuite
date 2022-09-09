d039df984df2168af9453f0376eb752b57c30657
BenceSzmola <0szmola.bence0@gmail.com>
2022-09-09 11:35:31 +0200

    Update getTxtPixelSize.m

--- getTxtPixelSize.m
+++ getTxtPixelSize.m
@@ -1,4 +1,4 @@
-function width = getTxtPixelSize(str,fontName,fontSize)
+function [width,heigth] = getTxtPixelSize(str,fontName,fontSize)
 
 if nargin > 1
     getPixSizeUIC = uicontrol('Style', 'text', 'Visible', 'off', 'FontName', fontName, 'FontSize', fontSize);
@@ -7,4 +7,7 @@ else
 end
 getPixSizeUIC.String = str;
 width = getPixSizeUIC.Extent(3);
+if nargout == 2
+    heigth = getPixSizeUIC.Extent(4);
+end
 delete(getPixSizeUIC)
