cc7d6e95cc2630cfa1c546ad876620179e0a1135
BenceSzmola <0szmola.bence0@gmail.com>
2022-09-13 16:05:57 +0200

    upgraded show rhd info stuff

--- showRHDinfo.m
+++ showRHDinfo.m
@@ -23,19 +23,41 @@ rhdInfoFig = figure('Name', 'RHD information',...
                     'Resize', 'off');
 
 
-[~,maxLenInds] = max(cellfun(@length, rhdInfoCell));
-wid1 = getTxtPixelSize(rhdInfoCell{maxLenInds(1),1});
-wid2 = getTxtPixelSize(rhdInfoCell{maxLenInds(2),2});
+[maxLen1,maxLenInds1] = max(cellfun(@length, rhdInfoCell{1}));
+[maxLen2,maxLenInds2] = max(cellfun(@length, rhdInfoCell{2}));
+if maxLen1(1) > maxLen2(1)
+    wid1 = getTxtPixelSize(rhdInfoCell{1}{maxLenInds1(1),1});
+else
+    wid1 = getTxtPixelSize(rhdInfoCell{2}{maxLenInds2(1),1});
+end
+if maxLen1(2) > maxLen2(2)
+    wid2 = getTxtPixelSize(rhdInfoCell{1}{maxLenInds1(2),2});
+else
+    wid2 = getTxtPixelSize(rhdInfoCell{2}{maxLenInds2(2),2});
+end
 
-colNames = {'Parameters','Values'};
-colWidths = {ceil(wid1)+30, ceil(wid2)+10};
-infoTab = uitable(rhdInfoFig,...
+colNames1 = {'Parameters','Values'};
+colWidths1 = {ceil(wid1)+30, ceil(wid2)+10};
+infoTabl1 = uitable(rhdInfoFig,...
                     'Units', 'normalized',...
-                    'Position', [0, 0, 1, 1],...
-                    'Data', rhdInfoCell,...
-                    'ColumnName', colNames,...
-                    'ColumnWidth', colWidths);
-rhdInfoFig.Position(3:4) = rhdInfoFig.Position(3:4).*infoTab.Extent(3:4);
+                    'Position', [0, 0.7, 1, 0.3],...
+                    'Data', rhdInfoCell{1},...
+                    'ColumnName', colNames1,...
+                    'ColumnWidth', colWidths1);
+                
+colNames2 = {'Channel names','Impedance magnitudes'};
+colWidths2 = {ceil(wid1)+30, ceil(wid2)+10};
+infoTabl2 = uitable(rhdInfoFig,...
+                    'Units', 'normalized',...
+                    'Position', [0, 0, 1, 0.7],...
+                    'Data', rhdInfoCell{2},...
+                    'ColumnName', colNames2,...
+                    'ColumnWidth', colWidths2);
+                
+                
+tablesExtent(1) = infoTabl1.Extent(3);
+tablesExtent(2) = infoTabl1.Extent(4) + infoTabl2.Extent(4);
+rhdInfoFig.Position(3:4) = rhdInfoFig.Position(3:4).*tablesExtent;
 rhdInfoFig.Visible = 'on';
 
 end
--- spectrogramMacher.m
+++ spectrogramMacher.m
@@ -14,31 +14,33 @@ if size(data,1) > size(data,2)
 end
 
 freqRange = freqLims(2)-freqLims(1);
-% if (w1 > 1000) | (w2 > 1000)
-%     errordlg('Unexpected frequency cutoff values!')
-%     return
-% end
 
-% dogged = DoG(data,fs,w1,w2);
 taxis = linspace(-round(length(data)*1000/(2*fs),4),round(length(data)*1000/(2*fs),4),...
     length(data));
 
 for i = 1:size(data,1)
-%     normdog = dogged+abs(min(dogged(i,:)));
-%     normdog = normdog/max(normdog);
     normData = data(i,:) + abs(min(data(i,:)));
     normData = normData / max(normData);
     [cfs,f] = cwt(data(i,:),'amor',fs,'FrequencyLimits',freqLims);
     cfs = abs(cfs);
     
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    freqsOI = (round(f) >= 150) & (round(f) <= 250);
+    %round(length(data)/2) - round(0.25*fs) : round(length(data)/2) + round(0.25*fs)
+    powInRange = sum(cfs(freqsOI,:), 'all');% / length(find(freqsOI));
+    powOuttaRange = sum(cfs(~freqsOI,:), 'all');% / length(find(~freqsOI));
+    powFullRange = sum(cfs, 'all');%/ length(f);
+    powInPerFull = powInRange / powFullRange
+    
+    [~, maxFreqs] = max(cfs,[],1);
+    avgFreq = mean(f(maxFreqs))
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
     figure('Name', 'CWT Spectrogram', 'NumberTitle', 'off')
-%     surface('XData',taxis,'YData',f,'CData',abs(cfs),...
-%         'ZData',zeros(size(cfs)),'EdgeColor','none','CDataMapping','scaled')
     surf(taxis,f,cfs,'EdgeColor','interp')
     view([0,90])
     hold on
 
-%     plot3(taxis,normdog*300+600,ones(size(taxis))*max(abs(cfs(:))),'-r')
     plot3(taxis, normData*(0.3*freqRange) + 0.6*freqRange, ones(size(taxis))*max(cfs(:)), '-r')
     
     if (nargin > 3) && ~isempty(detBorders)
