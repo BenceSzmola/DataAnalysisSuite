9e336333662fac3ecef17e6973a114e89e37808d
BenceSzmola <0szmola.bence0@gmail.com>
2022-11-04 14:30:48 +0100

    Update DAS.m

--- DAS.m
+++ DAS.m
@@ -3685,7 +3685,7 @@ classdef DAS < handle
             guiobj.ephys_proccedInfo = [guiobj.ephys_proccedInfo; newProcInfo];
             guiobj.ephys_procdatanames = procDatanames;
             guiobj.ephysProcListBox2.String = procDatanames;
-            guiobj.ephysProcListBox2.Value = length(procDatanames):-1:length(procDatanames)-(length(data_idx)-1);
+            guiobj.ephysProcListBox2.Value = length(procDatanames)-(length(data_idx)-1):length(procDatanames);
             ephysProcListBox2ValueChanged(guiobj)
             
             guiobj.ephysRunProcButton.BackgroundColor = 'g';
@@ -3865,7 +3865,7 @@ classdef DAS < handle
             guiobj.imaging_proccedInfo = [guiobj.imaging_proccedInfo; newProcInfo];
             guiobj.imaging_procDatanames = procDatanames;
             guiobj.imagingProcListBox2.String = procDatanames;
-            guiobj.imagingProcListBox2.Value = length(procDatanames):-1:length(procDatanames)-(length(data_idx)-1);
+            guiobj.imagingProcListBox2.Value = length(procDatanames)-(length(data_idx)-1):length(procDatanames);;
             imagingProcListBox2ValueChanged(guiobj)
             
             guiobj.imagingRunProcButton.BackgroundColor = 'g';
